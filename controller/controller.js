e536d74d9d962968382dff3483e0cd7cb0371592
John Davis <johnsdavis95@gmail.com>
2017-07-17 19:42:15 -0400

    got index page connected to server

---
+++ controller/controller.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var router = express.Router();
+
+console.log("Controller connected");
+
+// router.get("/", function(req, res) {
+// 	console.log("home page");
+// 	res.render("index");
+// })
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var exphbs = require('express-handlebars');
+
+var routes = require("./controller/controller.js");
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+
+app.set("view engine", "handlebars");
+
+// app.use("/", routes);
+
+app.listen(port, function() {
+
+	console.log("Port:", port);
+})
+
+app.get("/", function(req, res) {
+	console.log("home page");
+	res.render("index");
+})
+
